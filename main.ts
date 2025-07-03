56b5f904f77771bf66176586ea6264afd39cd34a
Marcos Candeia <marrcooos@gmail.com>
2025-07-03 12:58:12 -0300

    Adds workflow example
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -1,10 +1,41 @@
 import { DefaultEnv, withRuntime } from "@deco/workers-runtime";
+import { createStep, createWorkflow } from "@deco/workers-runtime/mastra";
+import { z } from "zod";
 import { Env as EnvGen } from "./env.gen.ts";
+
+const step1 = createStep({
+  execute: ({ inputData: { input } }) => {
+    console.log("running");
+    return Promise.resolve({
+      output: input + 1,
+    });
+  },
+  id: "sum",
+  inputSchema: z.object({
+    input: z.number(),
+  }),
+  outputSchema: z.object({
+    output: z.number(),
+  }),
+});
+
+const testWorkflow = () => createWorkflow({
+  id: "test-workflow",
+  description: "Test workflow",
+  inputSchema: z.object({
+    input: z.number(),
+  }),
+  outputSchema: z.object({
+    output: z.number(),
+  }),
+})
+  .then(step1)
+  .commit();
 
 export type Env = EnvGen & DefaultEnv;
 
-export default withRuntime<Env>({
-  async fetch(_req: Request, env: Env) {
-    return Response.json(await env.SLACK.LIST_CHANNELS({}));
-  },
+const { Workflow, ...workerAPIs } = withRuntime<Env>({
+  workflows: [testWorkflow],
 });
+export { Workflow };
+export default workerAPIs;
